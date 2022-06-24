9a6003f51c5a3031187fdcdcc674d8590cb1ba94
Vikdm <viktorov@my.com>
2022-06-24 22:45:54 +0300

    GameTouney v.1.1

--- src/test/java/ru/netology/domain/GameTest.java
+++ src/test/java/ru/netology/domain/GameTest.java
@@ -12,6 +12,8 @@ public class GameTest {
 
     Game game = new Game();
     Player player1 = new Player(11, "AAA", 11);
+    Player player11 = new Player(111, "AAAA", 11);
+
     Player player2 = new Player(22, "BBB", 22);
     Player player3 = new Player(33, "CCC", 33);
     Player player4 = new Player(44, "DDD", 44);
@@ -29,6 +31,30 @@ public class GameTest {
     }
 
     @Test
+    public void TestStr2() {
+        game.register(player1);
+        game.register(player11);
+
+
+        int actual = game.round("AAA", "AAAA");
+        int expected = 0;
+
+        assertEquals (actual, expected);
+    }
+
+    @Test
+    public void TestStr3() {
+        game.register(player4);
+        game.register(player11);
+
+
+        int actual = game.round("DDD", "AAAA");
+        int expected = 1;
+
+        assertEquals (actual, expected);
+    }
+
+    @Test
     public void TestError1() {
         game.register(player1);
         game.register(player2);
@@ -39,6 +65,19 @@ public class GameTest {
             game.round("123123A", "dsonggoianv");
         });
     }
+
+    @Test
+    public void TestError2() {
+        game.register(player1);
+        game.register(player2);
+        game.register(player4);
+
+
+        assertThrows(NotRegisteredException.class, () -> {
+            game.round("AAA", "dsonggoianv");
+        });
+    }
 }
+
 
 
